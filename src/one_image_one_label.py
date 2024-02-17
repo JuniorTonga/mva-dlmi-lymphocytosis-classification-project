9a7f64e538418d34e65df148056bb3d5064b4e42
Ben Kabongo <kabongo.ben025@gmail.com>
2024-02-17 17:01:34 +0100

    CNN One image one label

--- src/one_image_one_label.py
+++ src/one_image_one_label.py
@@ -11,13 +11,16 @@ from torch.utils.data import DataLoader, Dataset
 
 class ImageLabelDataset(Dataset):
 
-    def __init__(self, images, labels):
+    def __init__(self, images, labels, transform_function=None):
         self.images = images
         self.labels = labels
+        self.transform_function = transform_function
 
     def __getitem__(self, index):
         image_path = self.images[index]
         image = image_to_numpy(image_path)
+        if self.transform_function is not None:
+            image = self.transform_function(image)
         return image, self.labels[index]
 
     def __len__(self):
@@ -26,12 +29,15 @@ class ImageLabelDataset(Dataset):
 
 class ImageDataset(Dataset):
 
-    def __init__(self, images):
+    def __init__(self, images, transform_function=None):
         self.images = images
+        self.transform_function = transform_function
 
     def __getitem__(self, index):
         image_path = self.images[index]
         image = image_to_numpy(image_path)
+        if self.transform_function is not None:
+            image = self.transform_function(image)
         return image
 
     def __len__(self):
@@ -40,7 +46,7 @@ class ImageDataset(Dataset):
 
 def image_to_numpy(image_path):
     image_pil = Image.open(image_path)
-    image_np = np.array(image_pil)
+    image_np = np.array(image_pil, dtype=np.float32) / 255.0
     return image_np
 
 
@@ -64,14 +70,20 @@ def assign_labels(images_dict, labels_dict):
     images = []
     labels = []
 
-    for p, p_images in images_dict:
+    for p, p_images in images_dict.items():
         images.extend(p_images)
         labels.extend([labels_dict[p]] * len(p_images))
 
     return images, labels
 
 
-def predict(model, p_dataloader):
+def predict(model, p_dataloader, device):
     y_pred = []
-    for X in p_dataloader:
-        pass
+    model.eval()
+    with torch.no_grad():
+        for X in p_dataloader:
+            X = torch.FloatTensor(X).to(device)
+            logits = model(X)
+            pred = F.softmax(logits, dim=-1).argmax(dim=-1)
+            y_pred.extend(pred.cpu().detach().numpy().tolist())
+    return int(np.mean(y_pred) > .5)
